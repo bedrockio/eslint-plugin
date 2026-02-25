4496fc3a9bc0c0ac6f748ac19b28b4419041e86a
Andrew Plummer <plummer.andrew@gmail.com>
2026-02-25 21:18:18 +0900

    Ensure typescript plugin is actually optional

--- src/typescript.js
+++ src/typescript.js
@@ -1,15 +1,32 @@
-import ts from 'typescript-eslint';
+let ts;
+let importError;
+
+try {
+  // This is to make this package optional.
+  ts = await import('typescript-eslint');
+} catch (err) {
+  // Package not installed. Capture error to
+  // allow re-throwing if plugin is accessed,
+  // otherwise allow it to export silently.
+  importError = err;
+}
 
 export default {
   files: ['**/*.{ts,tsx}'],
   ignores: ['node_modules/**/*', '**/dist/**/*', '**/*.d.ts'],
   languageOptions: {
-    parser: ts.parser,
+    parser: ts?.parser,
   },
-  plugins: {
-    '@typescript-eslint': ts.plugin,
+  get plugins() {
+    if (importError) {
+      throw importError;
+    }
+    return {
+      '@typescript-eslint': ts?.plugin,
+    };
   },
   rules: {
-    ...ts.plugin.configs.recommended.rules,
+    ...ts?.plugin.configs.recommended.rules,
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
 };
