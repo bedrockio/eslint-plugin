17bdf1e9ce7aecadf9f55490fde0a9584348f751
Andrew Plummer <plummer.andrew@gmail.com>
2026-03-18 23:10:50 +0900

    Make sure ts plugin doesn't blow up projects that don't use it

--- src/typescript.js
+++ src/typescript.js
@@ -1,4 +1,11 @@
-import ts from 'typescript-eslint';
+let ts;
+let importError;
+
+try {
+  ts = (await import('typescript-eslint')).default;
+} catch (err) {
+  importError = err;
+}
 
 export default {
   name: 'typescript',
@@ -7,19 +14,28 @@ export default {
   languageOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    parser: ts.parser,
+    parser: ts?.parser,
   },
-  plugins: {
-    '@typescript-eslint': ts.plugin,
+
+  // Throw error if import failed only if
+  // plugin is actually being used.
+  get plugins() {
+    if (importError) {
+      throw importError;
+    }
+    return {
+      '@typescript-eslint': ts.plugin,
+    };
   },
+
   rules: {
     // Pull in `eslint-recommended` adjustments from typescript-eslint.
     // This disables core ESLint rules that conflict with TypeScript (e.g. no-undef).
-    ...ts.plugin.configs['eslint-recommended'].overrides[0].rules,
+    ...ts?.plugin.configs['eslint-recommended'].overrides[0].rules,
 
     // Pull in the TypeScript recommended rules themselves.
     // Avoid the flat config version as it's an array.
-    ...ts.plugin.configs.recommended.rules,
+    ...ts?.plugin.configs.recommended.rules,
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
 
