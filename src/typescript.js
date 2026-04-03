28fb753068db8b870e536ea0af2d914f7d3ab368
Andrew Plummer <plummer.andrew@gmail.com>
2026-04-03 16:25:54 +0900

    Simplified TS plugin

--- src/typescript.js
+++ src/typescript.js
@@ -1,11 +1,4 @@
-let ts;
-let importError;
-
-try {
-  ts = (await import('typescript-eslint')).default;
-} catch (err) {
-  importError = err;
-}
+import ts from 'typescript-eslint';
 
 export default {
   name: 'typescript',
@@ -14,28 +7,27 @@ export default {
   languageOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    parser: ts?.parser,
+    parser: ts.parser,
   },
-
-  // Throw error if import failed only if
-  // plugin is actually being used.
-  get plugins() {
-    if (importError) {
-      throw importError;
-    }
-    return {
-      '@typescript-eslint': ts.plugin,
-    };
+  plugins: {
+    '@typescript-eslint': ts.plugin,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
 
   rules: {
     // Pull in `eslint-recommended` adjustments from typescript-eslint.
     // This disables core ESLint rules that conflict with TypeScript (e.g. no-undef).
-    ...ts?.plugin.configs['eslint-recommended'].overrides[0].rules,
+    ...ts.plugin.configs['eslint-recommended'].overrides[0].rules,
 
     // Pull in the TypeScript recommended rules themselves.
     // Avoid the flat config version as it's an array.
-    ...ts?.plugin.configs.recommended.rules,
+    ...ts.plugin.configs.recommended.rules,
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
 
